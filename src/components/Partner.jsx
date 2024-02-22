34a911556966975806418216f5d367e575b50c93
yanu-setiawan <yanusetiawan777@gmail.com>
2024-02-22 16:01:12 +0700

    feat :add Partner

---
+++ src/components/Partner.jsx
@@ -0,0 +1,50 @@
+/* eslint-disable no-unused-vars */
+import React from "react";
+import bmw from "@brands/BMW.png";
+import honda from "@brands/Honda.png";
+import hyundai from "@brands/Hyundai.png";
+import kia from "@brands/KIA.png";
+import lexus from "@brands/Lexus.png";
+import marcedes from "@brands/Marcedes.png";
+import nissan from "@brands/Nissan.png";
+import toyota from "@brands/Toyota.png";
+
+const Partner = () => {
+  const brands = [
+    {
+      imgPath: bmw,
+    },
+    {
+      imgPath: honda,
+    },
+    {
+      imgPath: hyundai,
+    },
+    {
+      imgPath: kia,
+    },
+    {
+      imgPath: lexus,
+    },
+    {
+      imgPath: marcedes,
+    },
+    {
+      imgPath: nissan,
+    },
+    {
+      imgPath: toyota,
+    },
+  ];
+  return (
+    <>
+      <div className=" flex flex-wrap justify-center items-center gap-20 py-20 grayscale">
+        {brands.map((item, index) => (
+          <img src={item.imgPath} alt="p" key={index} />
+        ))}
+      </div>
+    </>
+  );
+};
+
+export default Partner;
