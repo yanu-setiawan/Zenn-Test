fb9600b2eec0a3a6f8604721e8907ae0990e2774
yanu-setiawan <yanusetiawan777@gmail.com>
2024-02-22 15:59:01 +0700

    feat :add Dashboard

---
+++ src/pages/Dashboard.jsx
@@ -0,0 +1,19 @@
+/* eslint-disable no-unused-vars */
+import React from "react";
+import Header from "../components/Header";
+import { Hero } from "../components/Hero";
+import GoRent from "../components/GoRent";
+import Partner from "../components/Partner";
+
+const Dashboard = () => {
+  return (
+    <>
+      <Header />
+      <Hero />
+      <GoRent />
+      <Partner />
+    </>
+  );
+};
+
+export default Dashboard;
