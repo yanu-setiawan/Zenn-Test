a10b72f1c1dec2aeb342ab003387e894b1dcd70d
yanu-setiawan <yanusetiawan777@gmail.com>
2024-02-22 16:01:41 +0700

    feat :add sample brand

---
+++ src/components/SampleBrand.jsx
@@ -0,0 +1,21 @@
+/* eslint-disable no-unused-vars */
+import React from "react";
+
+const SampleBrand = () => {
+  return (
+    <section>
+      <div className=" text-center flex flex-col justify-center gap-3">
+        <h2 className=" text-xl text-primary-0 ">Popular Car</h2>
+        <p className=" text-[2.7rem] text-softBlack-0">
+          Choose Your Suitable Car
+        </p>
+        <p className=" text-base text-softGrey-0">
+          We present popular cars that are rented by customers to maximize your
+          comfort on long trips.
+        </p>
+      </div>
+    </section>
+  );
+};
+
+export default SampleBrand;
